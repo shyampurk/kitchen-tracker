59e7fa87c73d0df6f280d8aa8fcbf5ad9f0a9153
Shyam Purkayastha <shyam@transceage.com>
2016-03-04 22:16:09 +0530

    Update settings.h

--- likitble/pubnub_linkit/settings.h
+++ likitble/pubnub_linkit/settings.h
@@ -7,7 +7,7 @@
 #define WIFI_AUTH LWIFI_WPA
 
 //Setting up the PubNub Publish and Subscribe Channels with Channel Name
-#define pubkey  "pub-c-913ab39c-d613-44b3-8622-2e56b8f5ea6d"
-#define subkey  "sub-c-8ad89b4e-a95e-11e5-a65d-02ee2ddab7fe"
+#define pubkey  "demo"
+#define subkey  "demo"
 
 #endif
